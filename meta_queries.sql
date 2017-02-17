ad258cc548409d21e1fe90216968f20588a7a320
grahamcrowell_vch <graham.crowell@gmail.com>
2017-02-17 08:04:05 -0800

    combined Odbc repos.

---
+++ meta_queries.sql
@@ -0,0 +1,12 @@
+﻿
+
+
+SELECT
+	sch.name AS schema_name
+	,tab.name AS table_name
+	,col.name AS column_name
+FROM sys.schemas AS sch
+JOIN sys.tables AS tab
+ON sch.schema_id = tab.schema_id
+JOIN sys.columns as col
+ON tab.object_id = col.object_id
