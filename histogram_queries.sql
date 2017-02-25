60597cddb41268b13ececc1184d97066d9de2cd9
grahamcrowell_laptop <graham.crowell@gmail.com>
2017-02-24 21:44:08 -0800

    TSQL column value histogram in sqlcmd script

---
+++ histogram_queries.sql
@@ -0,0 +1,41 @@
+﻿:setvar server_name "PC"
+:setvar database_name "WideWorldImportersDW"
+:setvar schema_name "Fact"
+:setvar table_name "Sale"
+:setvar column_name "[Description]"
+
+:connect $(server_name)
+USE $(database_name)
+GO
+
+-- table row count is denominator
+DECLARE @RowCount INT;
+SELECT 
+	@RowCount = COUNT(*)
+FROM $(schema_name).$(table_name)
+
+-- return this to c++
+;WITH value_histogram AS (
+SELECT 
+	$(column_name) AS ColumnValue
+	,COUNT(*) AS ValueFrequency
+	,1.*COUNT(*)/@RowCount AS ValueProportion
+FROM $(schema_name).$(table_name)
+GROUP BY $(column_name)
+)
+SELECT 
+	ColumnValue
+	,ValueFrequency
+	,ValueProportion
+	,CUME_DIST() OVER(ORDER BY ValueProportion ASC) AS ValueCumulativeProportion
+FROM value_histogram
+
+
+
+--SELECT 
+--	$(column_name) AS ColumnValue
+--	,COUNT(*) AS ValueFrequency
+--	,1.*COUNT(*)/@RowCount AS ValueProportion
+--FROM $(schema_name).$(table_name)
+--GROUP BY $(column_name)
+--ORDER BY COUNT(*);
